1e5717f17a0c5c0da1d5ac6d3412f14b11397e1f
Andrew <andreytihonov447@gmail.com>
2025-10-02 14:07:04 +0300

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render plain: "hello, world!\nЯ родился! ＼(^o^)／"
+    render plain: "hello, world!\nЯ родился! ＼(^o^)／\n 2.10.2025"
   end
   allow_browser versions: :modern
 end
